22f0f4863e07c14797c06ac9e5a158079897f93f
Kavipriya <mkavipriya470@gmail.com>
2025-09-11 13:52:49 +0530

    Update IMB-Project.py

--- IMB-Project.py
+++ IMB-Project.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1ZCtpuj7toS82vjmGsfEoPo4jyhl9iNWy
 """
 
-!pip install transformers torch gradio PyPDF2 -q
 
 !pip install transformers torch gradio PyPDF2 -q
 
